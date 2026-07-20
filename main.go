e2d8273466a334c03bd497b1cfc2bdda2b4f6bb2
Isvane <277444536+Isvane@users.noreply.github.com>
2026-07-20 13:34:29 +0700

    feat: add thread-safe in-memory database

--- main.go
+++ main.go
@@ -5,13 +5,24 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"sync"
 )
+
+type Database struct {
+	mu       sync.RWMutex
+	UserInfo map[string]int
+}
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.PathValue("arg")))
 }
 
 func main() {
+	db := &Database{
+		UserInfo: make(map[string]int),
+	}
+	_ = db
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /echo/{arg}", echoHandler)
